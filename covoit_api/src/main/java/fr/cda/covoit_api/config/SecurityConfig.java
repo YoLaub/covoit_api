eaee45c07dead2219e133fc1a6569463d72c3f52
YoLaub <johngreta904@gmail.com>
2026-03-23 15:54:45 +0100

    Update CORS configuration in `SecurityConfig` to change allowed origin URL for production

--- covoit_api/src/main/java/fr/cda/covoit_api/config/SecurityConfig.java
+++ covoit_api/src/main/java/fr/cda/covoit_api/config/SecurityConfig.java
@@ -114,7 +114,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:5173", "http://localhost:4200", "https://co-voit.john-world.store"));
+        configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:5173", "http://localhost:4200", "https://covoitogreta.john-world.store"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
